97374ac1acbc18fd2f2e0e7d1929ce45e7ab8ab8
zenstateofmind <nikhil.joshi92@gmail.com>
2016-06-25 17:14:56 +0530

    Added dagger dependency and basic infrastructure for testing with dagger with a couple LoginActivity tests

--- app/src/main/java/com/example/nikhiljoshi/enlighten/MyApplication.java
+++ app/src/main/java/com/example/nikhiljoshi/enlighten/MyApplication.java
@@ -3,6 +3,9 @@ package com.example.nikhiljoshi.enlighten;
 import android.app.Application;
 import android.content.Context;
 
+import com.example.nikhiljoshi.enlighten.dagger.component.BaseTwitterComponent;
+import com.example.nikhiljoshi.enlighten.dagger.component.DaggerTwitterComponent;
+import com.example.nikhiljoshi.enlighten.dagger.module.TwitterModule;
 import com.facebook.stetho.Stetho;
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
@@ -13,6 +16,16 @@ import io.fabric.sdk.android.Fabric;
  * Created by nikhiljoshi on 5/8/16.
  */
 public class MyApplication extends Application {
+
+    private final BaseTwitterComponent baseTwitterComponent = createBaseTwitterComponent();
+
+    protected BaseTwitterComponent createBaseTwitterComponent() {
+        return DaggerTwitterComponent.builder().twitterModule(new TwitterModule()).build();
+    }
+
+    public BaseTwitterComponent baseTwitterComponent() {
+        return baseTwitterComponent;
+    }
 
     /**
      * This is where {@link Twitter} gets instantiated.
